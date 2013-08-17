904979f233668d8fd52ba27a8cd0a35e643ece2b
Robbie Marcelo <rbmrclo@hotmail.com>
2013-08-18 00:37:13 +0800

    Initial commit with love <3 teehee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmberlockDemo::Application.config.secret_token = '0ff86949fd5acb75ba05ad2dfb684aa7943318dee87261a2a2910482d44ad605d3c48c780cff74d4d235e01284e333ce2a2549ecedb68a2d12695e838097ec2e'
